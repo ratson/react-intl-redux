cb6270aeb7df98f9f927aa60e4a40b550f063877
thibaut.sabot <thibaut.sabot@inovia-team.com>
2017-04-18 17:06:30 +0200

    use proptypes

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,5 +1,6 @@
 import {Provider as ReduxProvider} from 'react-redux'
-import React, {PropTypes} from 'react'
+import React from 'react'
+import PropTypes from 'prop-types';
 
 import IntlProvider from './IntlProvider'
 
